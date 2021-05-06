2676796869c4a4b339b5cd6608dee46d2a6fc24c
Ercan Art <ercan.artt@gmail.com>
2021-05-07 00:15:13 +0300

    Second commit

--- Kodlama.IoThirdDay/src/Main.java
+++ Kodlama.IoThirdDay/src/Main.java
@@ -8,16 +8,16 @@ public class Main {
 		Instructor instructor1 = new Instructor(3,"engin","demiroð","engin@gmail.com","Full Stack Developer");
 		Instructor instructor2 = new Instructor(4,"gökay","güneþ","gunes@gmail.com","Backend");
 		
-		NewUserI newUserI = new NewUserI(); //Yeni bir kullanýcý Manager eklersek kullanýrýz.
+		NewUserI newUserI = new NewUserI(); //Yeni bir kullanici Manager eklersek kullaniriz.
 		
 		newUserI.newUserAdd(new InstructorManager());
 		
-		StudentManager studentManager = new StudentManager(); //Öðrenci metotlarý için
+		StudentManager studentManager = new StudentManager(); //Ogrenci metotlarý icin
 		
 		studentManager.addStudent(student1);
 		studentManager.delete(student2);
 		
-		InstructorManager instructorManager = new InstructorManager(); //Eðitmen metotlarý için
+		InstructorManager instructorManager = new InstructorManager(); //Egitmen metotlarý icin
 		
 		instructorManager.delete(instructor2);
 		instructorManager.addInstructor(instructor1);
